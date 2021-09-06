7eef5d6e71897c7df7239742c0ca1b21137fd3d3
Badr <bsh20351badersh20002@gmail.com>
2021-09-06 22:56:03 +0200

    first commit

---
+++ Desktop/CV/cv/src/compnents/layout/navlinks/Hero.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Hero.css"
+
+
+const Hero = ()=> {
+
+    return(
+       
+<div id = "Home">
+
+   <div className = "heroContainer">
+
+           <h1 className = "MyName">
+                Badr shehim
+           </h1>
+
+         <p className = "parg">
+                a Full Stack Web Developer
+         </p>
+
+            <img alt = "#" className = "coffee"/>
+
+    </div>   
+       
+</div>
+   
+       
+    )
+
+
+
+
+
+}
+
+
+
+export default Hero
---
+++ Desktop/CV/cv/src/compnents/layout/navlinks/contact.js
@@ -0,0 +1,46 @@
+import React from "react"
+import "./contact.css"
+import github from "../../../Images/Github.png"  
+import facebook from "../../../Images/Facebook.png"
+import Google from "../../../Images/Google.png"
+import Phone from "../../../Images/Phone.png"
+
+
+const Footer = ()=> {
+    return(
+      <>
+      <div id = "contact">
+       <h1>Contact Me</h1>
+       <span><img id = "phone" src = {Phone} alt = "#"/><p>Phone Number [Almadar] : 091 012 46 49</p></span>
+       <br/>
+       <br/>
+       <span><img id = "phone" src = {Phone} alt = "#"/><p>Phone Mumber [Libyana] : 092 732 97 87</p></span>
+ 
+
+</div>
+
+        
+    <div className = "footer">
+
+    <span> <img src = {github} alt = "#"/><a target="_blank" rel="noreferrer" href = "https://github.com/BADR-DEEV?tab=repositories">Github </a> </span> 
+   <span> <img id = "F" src = {facebook} alt = "#"/><a target="_blank" rel="noreferrer" href = "https://www.facebook.com/profile.php?id=100005412978012">Facebook </a> </span> 
+   <span> <img id = "G" src = {Google} alt = "#"/><a className = "ga" href="mailto:bsh20351badersh20002@gmail.com">Gmail </a> </span> 
+
+
+
+
+
+    
+    </div>
+    </>
+    )
+
+
+
+
+
+}
+
+
+
+export default Footer
